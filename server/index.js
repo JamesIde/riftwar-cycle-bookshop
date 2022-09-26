404c5e4fb93d06842cdfaaf33a4a53177c16dc84
James <james.ide775@gmail.com>
2022-09-26 15:36:53 +0930

    More fixes

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ require("dotenv").config()
 
 const express = require("express")
 const app = express()
-const port = process.env.PORT
+const port = process.env.PORT || 3000
 const path = require("path")
 const colors = require("colors")
 const dbConnect = require("./db/dbConnect")
